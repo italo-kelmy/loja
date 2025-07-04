729411015e0eb86a5826fa3c4d15362dfc41861c
italokelmy12 <italokelmy1122@hotmail.com>
2025-07-04 15:38:19 -0300

    correção

--- loja/src/main/java/com/loja_de_eletronicos/loja/LojaApplication.java
+++ loja/src/main/java/com/loja_de_eletronicos/loja/LojaApplication.java
@@ -2,12 +2,12 @@ package com.loja_de_eletronicos.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 public class LojaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LojaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LojaApplication.class, args);
+    }
 }
--- loja/src/main/java/com/loja_de_eletronicos/loja/Repository/CarrinhoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.loja_de_eletronicos.loja.Repository;
-
-import com.loja_de_eletronicos.loja.Entity.Carrinho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
-    long countByUsuarioId(Long usuarioId);
-}
